5cc7546e312e339041af4a219ec1245de0aaf806
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-09-21 16:17:56 -0600

    add package config

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
 const mongoose = require('mongoose');
-
-const _configs = {
-    username: "",
-    password: "",
-    host: "",
-    port: "",
-    database: ""
-};
-
+const { MongoConfig } = require("configs-twtg");
 const _getUri = () => "mongodb://" +
-    `${_configs.username}:` +
-    `${_configs.password}@` +
-    `${_configs.host}:` +
-    `${_configs.port}/` +
-    `${_configs.database}`;
+    `${MongoConfig.username}:` +
+    `${MongoConfig.password}@` +
+    `${MongoConfig.host}:` +
+    `${MongoConfig.port}/` +
+    `${MongoConfig.database}`;
 
 class TwtgOdm{
-    constructor(configdb){
-        const jsv = require("json-validator");
-        const {configValidator} = require("./validators");
-        const validators = jsv.validate(configdb, configValidator);
-        if(validators.length > 0)
-            throw new Error(validators);
-        _configs.database  = configdb.database;
-        _configs.host  = configdb.host;
-        _configs.password  = configdb.password;
-        _configs.username  = configdb.username;
-        _configs.port  = configdb.port;
+    constructor(){
         this.db = require("./models");
     }
 
